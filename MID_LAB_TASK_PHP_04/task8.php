d17ee9455e2c664282797a544024c6ac22ab84ae
OishorjoAgnes <74955056+OishorjoAgnes@users.noreply.github.com>
2021-02-22 00:45:41 +0600

    Add files via upload

---
+++ MID_LAB_TASK_PHP_04/task8.php
@@ -0,0 +1,33 @@
+<?php
+
+	$elements = [
+				[1, 2, 3, 'A'],
+				[1, 2, 'B', 'C'],
+				[1, 'D', 'E', 'F']
+			];
+	
+	$elements_no = count($elements);
+	
+	for($row = 0; $row < $elements_no; $row++){
+		$row_elements = count($elements[$row]);
+		for($col = 0; $col < $row_elements; $col++){
+			if($elements[$row][$col] >= 'A' && $elements[$row][$col] <= 'Z'){
+				break;
+			}
+			echo $elements[$row][$col];
+		}
+		echo "<br>";
+	}
+	
+	echo "<br>";
+	
+	for($row = 0; $row < $elements_no; $row++){
+		$row_elements = count($elements[$row]);
+		for($col = 0; $col < $row_elements; $col++){
+			if($elements[$row][$col] >= 'A' && $elements[$row][$col] <= 'Z'){
+				echo $elements[$row][$col];
+			}
+		}
+		echo "<br>";
+	}
+	
